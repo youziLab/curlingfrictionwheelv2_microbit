df05b517cfeb59616dfc6852d4cce24e057cc3e2
youzi <youzibycc@gmail.com>
2025-03-14 17:13:53 +0800

    改速度逻辑BUG

--- main.ts
+++ main.ts
@@ -8,29 +8,17 @@ function 常量初始化 () {
 }
 function 遥控控制 () {
     if (PlanetX_Basic.get_Attention_Value(PlanetX_Basic.value_level.Tri)) {
-        最大挡位速度 = 30
-        nezhaV2.start(nezhaV2.MotorPostion.M3, 最大挡位速度 * -1)
-        nezhaV2.start(nezhaV2.MotorPostion.M4, 最大挡位速度 * 1)
-        局部_内转标记 = 0
-    } else {
-        if (局部_内转标记 == 0) {
-            nezhaV2.start(nezhaV2.MotorPostion.M3, 0)
-            nezhaV2.start(nezhaV2.MotorPostion.M4, 0)
-        }
-    }
-    if (PlanetX_Basic.get_Attention_Value(PlanetX_Basic.value_level.Squ)) {
-        最大挡位速度 = 60
-        nezhaV2.start(nezhaV2.MotorPostion.M3, 最大挡位速度 * -1)
-        nezhaV2.start(nezhaV2.MotorPostion.M4, 最大挡位速度 * 1)
-        局部_内转标记 = 0
-    } else {
-        if (局部_内转标记 == 0) {
-            nezhaV2.start(nezhaV2.MotorPostion.M3, 0)
-            nezhaV2.start(nezhaV2.MotorPostion.M4, 0)
-        }
-    }
-    if (PlanetX_Basic.get_Attention_Value(PlanetX_Basic.value_level.Cir)) {
         最大挡位速度 = 50
+        nezhaV2.start(nezhaV2.MotorPostion.M3, 最大挡位速度 * -1)
+        nezhaV2.start(nezhaV2.MotorPostion.M4, 最大挡位速度 * 1)
+        局部_内转标记 = 0
+    } else if (PlanetX_Basic.get_Attention_Value(PlanetX_Basic.value_level.Squ)) {
+        最大挡位速度 = 80
+        nezhaV2.start(nezhaV2.MotorPostion.M3, 最大挡位速度 * -1)
+        nezhaV2.start(nezhaV2.MotorPostion.M4, 最大挡位速度 * 1)
+        局部_内转标记 = 0
+    } else if (PlanetX_Basic.get_Attention_Value(PlanetX_Basic.value_level.Cir)) {
+        最大挡位速度 = 100
         nezhaV2.start(nezhaV2.MotorPostion.M3, 最大挡位速度 * -1)
         nezhaV2.start(nezhaV2.MotorPostion.M4, 最大挡位速度 * 1)
         局部_内转标记 = 0
