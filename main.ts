cdb311635c35928787966bf2bd85055e00a01365
youzi <youzibycc@gmail.com>
2025-03-14 18:23:36 +0800

    增加了缓启 缓停

--- main.ts
+++ main.ts
@@ -4,35 +4,54 @@ function 常量初始化 () {
     常量_舵机_上臂关角度 = 45
     常量_推杆时间 = 500
     最大挡位速度 = 0
+    缓启停步进时间ms = 10
     basic.showIcon(IconNames.Chessboard)
 }
 function 遥控控制 () {
     if (PlanetX_Basic.get_Attention_Value(PlanetX_Basic.value_level.Tri)) {
-        最大挡位速度 = 50
-        nezhaV2.start(nezhaV2.MotorPostion.M3, 最大挡位速度 * -1)
-        nezhaV2.start(nezhaV2.MotorPostion.M4, 最大挡位速度 * 1)
+        最大挡位速度 = 100
         局部_内转标记 = 0
     } else if (PlanetX_Basic.get_Attention_Value(PlanetX_Basic.value_level.Squ)) {
-        最大挡位速度 = 80
-        nezhaV2.start(nezhaV2.MotorPostion.M3, 最大挡位速度 * -1)
-        nezhaV2.start(nezhaV2.MotorPostion.M4, 最大挡位速度 * 1)
+        最大挡位速度 = 60
         局部_内转标记 = 0
     } else if (PlanetX_Basic.get_Attention_Value(PlanetX_Basic.value_level.Cir)) {
-        最大挡位速度 = 100
-        nezhaV2.start(nezhaV2.MotorPostion.M3, 最大挡位速度 * -1)
-        nezhaV2.start(nezhaV2.MotorPostion.M4, 最大挡位速度 * 1)
+        最大挡位速度 = 80
         局部_内转标记 = 0
     } else {
         if (局部_内转标记 == 0) {
-            nezhaV2.start(nezhaV2.MotorPostion.M3, 0)
-            nezhaV2.start(nezhaV2.MotorPostion.M4, 0)
+            if (最大挡位速度 > 0) {
+                最大挡位速度 = 0
+            }
         }
     }
     if (PlanetX_Basic.get_Attention_Value(PlanetX_Basic.value_level.X)) {
-        最大挡位速度 = 50
-        nezhaV2.start(nezhaV2.MotorPostion.M3, 最大挡位速度 * 1)
-        nezhaV2.start(nezhaV2.MotorPostion.M4, 最大挡位速度 * -1)
+        if (最大挡位速度 > 0 || 局部_内转标记 == 0) {
+            最大挡位速度 = -50
+        }
         局部_内转标记 = 1
+    }
+    if (最大挡位速度 != 缓慢启停速度) {
+        if (Math.abs(最大挡位速度 - 缓慢启停速度) < 10) {
+            缓慢启停速度 = 最大挡位速度
+        }
+        if (control.millis() - 缓启停时间标志位 > 缓启停步进时间ms) {
+            缓启停时间标志位 = control.millis()
+            if (Math.abs(缓慢启停速度) < 3) {
+                if (最大挡位速度 > 缓慢启停速度) {
+                    缓慢启停速度 += 1
+                } else {
+                    缓慢启停速度 += -1
+                }
+            } else {
+                if (最大挡位速度 > 缓慢启停速度) {
+                    缓慢启停速度 += 3
+                } else {
+                    缓慢启停速度 += -3
+                }
+            }
+        }
+        nezhaV2.start(nezhaV2.MotorPostion.M3, 缓慢启停速度 * -1)
+        nezhaV2.start(nezhaV2.MotorPostion.M4, 缓慢启停速度 * 1)
     }
 }
 function 启动初始化 () {
@@ -75,7 +94,10 @@ let 局部_RC_Y = 0
 let 速度解算局部_MAX = 0
 let 速度解算局部_v2 = 0
 let 速度解算局部_v1 = 0
+let 缓启停时间标志位 = 0
+let 缓慢启停速度 = 0
 let 局部_内转标记 = 0
+let 缓启停步进时间ms = 0
 let 最大挡位速度 = 0
 let 常量_推杆时间 = 0
 let 常量_舵机_上臂关角度 = 0
